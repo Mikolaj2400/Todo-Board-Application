e8fd11a2e1ca6b1cbc57170411ecf4e65a7845b7
Mikolaj2400 <mikka2000@interia.pl>
2023-04-17 19:10:26 +0200

    Adding icons

--- client/src/components/Tasks.js
+++ client/src/components/Tasks.js
@@ -302,15 +302,26 @@ const Tasks = () => {
                             <br/>
                             {!task.completed && 
                             <div>
-                                <button data-tooltip="Wykonaj" data-placement="top" onClick={() => handleCompleteTask(task)} className="btn-tasks">✓</button>
-                                <button data-tooltip="Edytuj" data-placement="top" onClick={() => handleEditTask(task._id)} className="btn-tasks">✎</button>
-                                <button data-tooltip="Usuń" data-placement="top" onClick={() => handleDeleteTask(task._id)} className="btn-tasks">🗑</button>               
+                                <button data-tooltip="Wykonaj" data-placement="top" onClick={() => handleCompleteTask(task)} className="btn-tasks">
+                                    <svg className="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="check"><path fill="#FFFFFF" d="M18.71,7.21a1,1,0,0,0-1.42,0L9.84,14.67,6.71,11.53A1,1,0,1,0,5.29,13l3.84,3.84a1,1,0,0,0,1.42,0l8.16-8.16A1,1,0,0,0,18.71,7.21Z"></path></svg>
+                                </button>
+                                <button data-tooltip="Edytuj" data-placement="top" onClick={() => handleEditTask(task._id)} className="btn-tasks">
+                                    <svg className="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="pen"><path fill="#FFFFFF" d="M22,7.24a1,1,0,0,0-.29-.71L17.47,2.29A1,1,0,0,0,16.76,2a1,1,0,0,0-.71.29L13.22,5.12h0L2.29,16.05a1,1,0,0,0-.29.71V21a1,1,0,0,0,1,1H7.24A1,1,0,0,0,8,21.71L18.87,10.78h0L21.71,8a1.19,1.19,0,0,0,.22-.33,1,1,0,0,0,0-.24.7.7,0,0,0,0-.14ZM6.83,20H4V17.17l9.93-9.93,2.83,2.83ZM18.17,8.66,15.34,5.83l1.42-1.41,2.82,2.82Z"></path></svg>
+                                </button>
+                                <button data-tooltip="Usuń" data-placement="top" onClick={() => handleDeleteTask(task._id)} className="btn-tasks">
+                                    <svg className="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="trash-alt"><path fill="#FFFFFF" d="M10,18a1,1,0,0,0,1-1V11a1,1,0,0,0-2,0v6A1,1,0,0,0,10,18ZM20,6H16V5a3,3,0,0,0-3-3H11A3,3,0,0,0,8,5V6H4A1,1,0,0,0,4,8H5V19a3,3,0,0,0,3,3h8a3,3,0,0,0,3-3V8h1a1,1,0,0,0,0-2ZM10,5a1,1,0,0,1,1-1h2a1,1,0,0,1,1,1V6H10Zm7,14a1,1,0,0,1-1,1H8a1,1,0,0,1-1-1V8H17Zm-3-1a1,1,0,0,0,1-1V11a1,1,0,0,0-2,0v6A1,1,0,0,0,14,18Z"></path></svg>
+                                </button>     
                             </div>}
 
                             {task.completed && 
                             <div>
-                                <button data-tooltip="Przywróć" data-placement="top" onClick={() => handleCompleteTask(task)} className="btn-tasks">X</button>
-                                <button data-tooltip="Usuń" data-placement="top" onClick={() => handleDeleteTask(task._id)} className="btn-tasks">🗑</button>
+                                <button data-tooltip="Przywróć" data-placement="top" onClick={() => handleCompleteTask(task)} className="btn-tasks">
+                                    <svg className="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="redo"><path fill="#FFFFFF" d="M21,11a1,1,0,0,0-1,1,8.05,8.05,0,1,1-2.22-5.5h-2.4a1,1,0,0,0,0,2h4.53a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4.77A10,10,0,1,0,22,12,1,1,0,0,0,21,11Z"></path></svg>
+                                </button>
+
+                                <button data-tooltip="Usuń" data-placement="top" onClick={() => handleDeleteTask(task._id)} className="btn-tasks">
+                                    <svg className="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="trash-alt"><path fill="#FFFFFF" d="M10,18a1,1,0,0,0,1-1V11a1,1,0,0,0-2,0v6A1,1,0,0,0,10,18ZM20,6H16V5a3,3,0,0,0-3-3H11A3,3,0,0,0,8,5V6H4A1,1,0,0,0,4,8H5V19a3,3,0,0,0,3,3h8a3,3,0,0,0,3-3V8h1a1,1,0,0,0,0-2ZM10,5a1,1,0,0,1,1-1h2a1,1,0,0,1,1,1V6H10Zm7,14a1,1,0,0,1-1,1H8a1,1,0,0,1-1-1V8H17Zm-3-1a1,1,0,0,0,1-1V11a1,1,0,0,0-2,0v6A1,1,0,0,0,14,18Z"></path></svg>
+                                </button>
                                 <p>Zadanie wykonane</p>
                             </div>}
                             <hr />
@@ -348,14 +359,25 @@ const Tasks = () => {
                             <br />
                             {!task.completed &&
                             <div>
-                                <button data-tooltip="Wykonaj" data-placement="top" onClick={() => handleCompleteTask(task)} className="btn-tasks">✓</button>
-                                <button data-tooltip="Edytuj" data-placement="top" onClick={() => handleEditHabit(task._id)} className="btn-tasks">✎</button>
-                                <button data-tooltip="Usuń" data-placement="top" onClick={() => handleDeleteTask(task._id)} className="btn-tasks">🗑</button>
+                                <button data-tooltip="Wykonaj" data-placement="top" onClick={() => handleCompleteTask(task)} className="btn-tasks">
+                                    <svg className="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="check"><path fill="#FFFFFF" d="M18.71,7.21a1,1,0,0,0-1.42,0L9.84,14.67,6.71,11.53A1,1,0,1,0,5.29,13l3.84,3.84a1,1,0,0,0,1.42,0l8.16-8.16A1,1,0,0,0,18.71,7.21Z"></path></svg>
+                                </button>
+                                <button data-tooltip="Edytuj" data-placement="top" onClick={() => handleEditHabit(task._id)} className="btn-tasks">
+                                    <svg className="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="pen"><path fill="#FFFFFF" d="M22,7.24a1,1,0,0,0-.29-.71L17.47,2.29A1,1,0,0,0,16.76,2a1,1,0,0,0-.71.29L13.22,5.12h0L2.29,16.05a1,1,0,0,0-.29.71V21a1,1,0,0,0,1,1H7.24A1,1,0,0,0,8,21.71L18.87,10.78h0L21.71,8a1.19,1.19,0,0,0,.22-.33,1,1,0,0,0,0-.24.7.7,0,0,0,0-.14ZM6.83,20H4V17.17l9.93-9.93,2.83,2.83ZM18.17,8.66,15.34,5.83l1.42-1.41,2.82,2.82Z"></path></svg>
+                                </button>
+                                <button data-tooltip="Usuń" data-placement="top" onClick={() => handleDeleteTask(task._id)} className="btn-tasks">
+                                    <svg className="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="trash-alt"><path fill="#FFFFFF" d="M10,18a1,1,0,0,0,1-1V11a1,1,0,0,0-2,0v6A1,1,0,0,0,10,18ZM20,6H16V5a3,3,0,0,0-3-3H11A3,3,0,0,0,8,5V6H4A1,1,0,0,0,4,8H5V19a3,3,0,0,0,3,3h8a3,3,0,0,0,3-3V8h1a1,1,0,0,0,0-2ZM10,5a1,1,0,0,1,1-1h2a1,1,0,0,1,1,1V6H10Zm7,14a1,1,0,0,1-1,1H8a1,1,0,0,1-1-1V8H17Zm-3-1a1,1,0,0,0,1-1V11a1,1,0,0,0-2,0v6A1,1,0,0,0,14,18Z"></path></svg>
+                                </button>
                             </div>}
                             {task.completed && 
                                 <div>
-                                    <button data-tooltip="Przywróć" data-placement="top" onClick={() => handleCompleteTask(task)} className="btn-tasks">X</button>
-                                    <button data-tooltip="Usuń" data-placement="top" onClick={() => handleDeleteTask(task._id)} className="btn-tasks">🗑</button>
+                                    <button data-tooltip="Przywróć" data-placement="top" onClick={() => handleCompleteTask(task)} className="btn-tasks">
+                                        <svg className="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="redo"><path fill="#FFFFFF" d="M21,11a1,1,0,0,0-1,1,8.05,8.05,0,1,1-2.22-5.5h-2.4a1,1,0,0,0,0,2h4.53a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4.77A10,10,0,1,0,22,12,1,1,0,0,0,21,11Z"></path></svg>
+                                    </button>
+
+                                    <button data-tooltip="Usuń" data-placement="top" onClick={() => handleDeleteTask(task._id)} className="btn-tasks">
+                                        <svg className="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="trash-alt"><path fill="#FFFFFF" d="M10,18a1,1,0,0,0,1-1V11a1,1,0,0,0-2,0v6A1,1,0,0,0,10,18ZM20,6H16V5a3,3,0,0,0-3-3H11A3,3,0,0,0,8,5V6H4A1,1,0,0,0,4,8H5V19a3,3,0,0,0,3,3h8a3,3,0,0,0,3-3V8h1a1,1,0,0,0,0-2ZM10,5a1,1,0,0,1,1-1h2a1,1,0,0,1,1,1V6H10Zm7,14a1,1,0,0,1-1,1H8a1,1,0,0,1-1-1V8H17Zm-3-1a1,1,0,0,0,1-1V11a1,1,0,0,0-2,0v6A1,1,0,0,0,14,18Z"></path></svg>
+                                    </button>
                                     <p>Nawyk wykonany!</p>
                                 </div>}
                             <hr />
